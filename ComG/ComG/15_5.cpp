3193c92b533a1f319b9a49703ec582be57ed33ed
stuzeakd <stuzeakd@nhnnext.org>
2014-05-15 12:40:55 +0900

    Log?

---
+++ ComG/ComG/15_5.cpp
@@ -0,0 +1,27 @@
+#include<windows.h>
+#include<gl/glut.h>
+
+void RenderScene(void){
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+
+}
+
+void SetupRC(void){
+
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+
+}
+
+void main(void){
+
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutCreateWindow("Simple");
+	glutDisplayFunc(RenderScene);
+	SetupRC();
+	glutMainLoop();
+
+}
+
+
